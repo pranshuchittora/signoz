af3272115c3914f4b61cf4741c31ac3f853d7327
Pranshu Chittora <pranshu@signoz.io>
2022-04-05 11:38:13 +0530

    chore: fix failing test cases

--- frontend/src/container/TraceFlameGraph/__tests__/TraceFlameGraph.test.tsx
+++ frontend/src/container/TraceFlameGraph/__tests__/TraceFlameGraph.test.tsx
@@ -7,36 +7,38 @@ test('loads and displays greeting', () => {
 	const onSpanHover = useState('');
 
 	const { asFragment } = render(
-		<TraceFlameGraph
-			{...{
-				hoveredSpanId: '',
-				intervalUnit: { multiplier: 0, name: 'm' },
-				onSpanHover: onSpanHover[1],
-				onSpanSelect: (): void => {},
-				selectedSpanId: '',
-				traceMetaData: {
-					globalEnd: 0,
-					globalStart: 0,
-					levels: 0,
-					spread: 0,
-					totalSpans: 0,
-				},
-				treeData: {
-					children: [],
-					id: '',
-					name: '',
-					serviceColour: '',
-					serviceName: '',
-					startTime: 0,
-					tags: [],
-					time: 0,
-					value: 0,
-					event: [],
-					hasError: false,
-					parent: undefined,
-				},
-			}}
-		/>,
+		<Provider store={store}>
+			<TraceFlameGraph
+				{...{
+					hoveredSpanId: '',
+					intervalUnit: { multiplier: 0, name: 'm' },
+					onSpanHover: rerender,
+					onSpanSelect: (): void => {},
+					selectedSpanId: '',
+					traceMetaData: {
+						globalEnd: 0,
+						globalStart: 0,
+						levels: 0,
+						spread: 0,
+						totalSpans: 0,
+					},
+					treeData: {
+						children: [],
+						id: '',
+						name: '',
+						serviceColour: '#a0e',
+						serviceName: '',
+						startTime: 0,
+						tags: [],
+						time: 100,
+						value: 100,
+						event: [],
+						hasError: false,
+						parent: undefined,
+					},
+				}}
+			/>
+		</Provider>,
 	);
 	expect(asFragment()).toMatchSnapshot();
 });
